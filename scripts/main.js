34db42abd6423a0fe99113df987e7b690a976dc5
MykolaDekusar <ndekusar97@gmail.com>
2024-05-10 17:36:21 +0200

    Changed calcolo variable location from main scope to function scope

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,9 @@
 'use strict';
 let numero = '';
 let primoNumero = '';
-let calcolo = 0;
 let operation = '';
 const risultato = document.getElementById("result");
 const buttons = document.querySelector('.tastierino');
-
 buttons.addEventListener('click', function (evento) {
     const valoreId = evento.target.innerText;
     //trasmetto alla funzione il valore del mio click
@@ -15,49 +13,55 @@ buttons.addEventListener('click', function (evento) {
 function logica(valore) {
     switch (valore) {
         default:
+            //verifico che non stia cliccando sulla board
             if (!isNaN(Number(valore))) {
                 numero += valore;
                 risultato.innerText = numero;
             }
             break;
         case '=':
-            risultato.innerText = operazione(operation);
+            //inizializzo la funzione passando il valore di operation (operatore)
             numero = operazione(operation);
-            console.log(numero);
+            risultato.innerText = numero;
             operation = '';
             primoNumero = '';
             break;
+        //scuoto tutti i campi //RESET
         case 'c':
             numero = '';
             primoNumero = '';
-            calcolo = 0;
             operation = '';
             risultato.innerText = '0';
             break;
         case '+':
             primoNumero += numero;
             numero = '';
+            //passo il valore + all'operatore
             operation += valore;
             break;
         case '-':
             primoNumero += numero;
             numero = '';
+            //passo il valore - all'operatore
             operation += valore;
             break;
         case 'x':
             primoNumero += numero;
             numero = '';
+            //passo il valore x all'operatore
             operation += valore;
             break;
         case '/':
             primoNumero += numero;
             numero = '';
+            //passo il valore / all'operatore
             operation += valore;
             break;
     }
 }
 
 function operazione(valore) {
+    let calcolo = 0;
     switch (valore) {
         case ('+'):
             calcolo = Number(primoNumero) + Number(numero);
